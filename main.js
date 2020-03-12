1f90efe684a8198ed3da8373e08991a3616b9841
Perriguey <yo44prg@icloud.com>
2020-03-12 13:21:22 +0100

    display tab

--- main.js
+++ main.js
@@ -6,7 +6,31 @@ var playerOneChar = "x";
 var playerTwoChar = "o";
 
 puissance4 = initTab(nbLigne, nbColonne, 0);
-displayTab(puissance4);
+displayTab(puissance4, playerOneChar, playerTwoChar);
+puissance4[3][3] = 1;
+puissance4[4][4] = 2;
+displayTab(puissance4, playerOneChar, playerTwoChar);
+
+
+while(true) {
+    if(playCase(1)) {
+        console.log("le joueur 1 a gagné");
+        break;
+    }
+    if(playCase(2)) {
+        console.log("le joueur 2 a gagné");
+        break;
+    }
+}
+
+function playCase(players) {
+    console.log("Le joueur %d joue", players);
+    if(players === 1) {
+        return true;
+    }
+    return false;
+}
+
 
 /**
  * permet d'initialiser un tab en fonction des ligne et des colonnes
@@ -28,9 +52,11 @@ function initTab(nbLigne, nbColonne, char = '') {
 
 /**
  * affiche un tableau de puissance 4
- * @param {Array<String>} tab 
+ * @param {Array<string>} tab 
+ * @param {string} playerOneChar 
+ * @param {string} playerTwoChar 
  */
-function displayTab(tab) {
+function displayTab(tab, playerOneChar, playerTwoChar) {
     for(var i = 0; i < tab.length; i++) {
         var txt = "";
         for(var j = 0; j < tab[i].length; j++) {
