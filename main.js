a0ecbbb4b4eb1db90fcd7ffab44aa7200c7b2c91
Perriguey <yo44prg@icloud.com>
2020-03-13 15:14:30 +0100

    add function play

--- main.js
+++ main.js
@@ -1,75 +1,50 @@
-
-var puissance4 = [];
-var nbColonne = 7;
-var nbLigne = 6;
-var playerOneChar = "x";
-var playerTwoChar = "o";
-
-puissance4 = initTab(nbLigne, nbColonne, 0);
-displayTab(puissance4, playerOneChar, playerTwoChar);
-puissance4[3][3] = 1;
-puissance4[4][4] = 2;
-displayTab(puissance4, playerOneChar, playerTwoChar);
+var toolbox = require("./toolbox.js");
+var power4 = require("./power4.js");
 
 
-while(true) {
-    if(playCase(1)) {
+intro();
+power4.playerOneChar = chooseChar(1);
+power4.playerTwoChar = chooseChar(2);
+power4.init();
+power4.displayTab();
+
+while (true) {
+    if (playCase(1)) {
         console.log("le joueur 1 a gagné");
         break;
     }
-    if(playCase(2)) {
+    if (playCase(2)) {
         console.log("le joueur 2 a gagné");
         break;
     }
 }
 
+function intro() {
+    var txt = "\n\n**********************************************************\n";
+    txt += "**************Bienvenue sur puissance 4*******************\n";
+    txt += "**********************************************************\n\n";
+    console.log(txt);
+}
+
+function chooseChar(players) {
+    var txt = "Veuillez choisir le caractere que vous voulez pour jouer" + players + " :" ;
+    return toolbox.writeString(txt);
+}
+
+/**
+ * fonction permettant a un joueur de jouer une case
+ * Retourne true si le joueur a gagné 
+ * @param {number} players 
+ */
 function playCase(players) {
-    console.log("Le joueur %d joue", players);
-    if(players === 1) {
-        return true;
+    var lineEmpty = -1;
+    var column = -1;
+    while (lineEmpty === -1 || column <= 0 || column > 7) {
+        console.log("choisir une colonne à un emplacement vide");
+        var column = power4.chooseColumn();
+        var lineEmpty = power4.returnLineCaseEmpty(column);
     }
-    return false;
-}
-
-
-/**
- * permet d'initialiser un tab en fonction des ligne et des colonnes
- * @param {number} nbLigne 
- * @param {number} nbColonne 
- * @param {*} char 
- */
-function initTab(nbLigne, nbColonne, char = '') {
-    var tab = [];
-    for(var i = 0; i < nbLigne; i++) {
-        var ligne = [];
-        for(var j = 0; j < nbColonne; j++) {
-            ligne.push(char);
-        }
-        tab.push(ligne);
-    }
-    return tab;
-}
-
-/**
- * affiche un tableau de puissance 4
- * @param {Array<string>} tab 
- * @param {string} playerOneChar 
- * @param {string} playerTwoChar 
- */
-function displayTab(tab, playerOneChar, playerTwoChar) {
-    for(var i = 0; i < tab.length; i++) {
-        var txt = "";
-        for(var j = 0; j < tab[i].length; j++) {
-            txt += "| ";
-            if(tab[i][j] === 0) {
-                txt += "_";
-            } else if(tab[i][j] === 1) {
-                txt += playerOneChar;
-            } else if(tab[i][j] === 2) {
-                txt += playerTwoChar;
-            }
-            txt += " |";
-        }
-        console.log(txt);
-    }
+    power4.playCase(players, lineEmpty, column);
+    power4.displayTab();
+    return power4.verifyEndGame(players);
 }
