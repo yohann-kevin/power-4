e19e19dfdbf277b5ac9f35fb55d42e0cbffee806
Perriguey <yo44prg@icloud.com>
2020-03-17 21:42:01 +0100

    add Interface 1

--- script/main.js
+++ script/main.js
@@ -7,15 +7,16 @@ power4.displayTab();
  * Retourne true si le joueur a gagné 
  * @param {number} players 
  */
-function playCase(players) {
-    var lineEmpty = -1;
-    var column = -1;
-    while (lineEmpty === -1 || column <= 0 || column > 7) {
-        console.log("choisir une colonne à un emplacement vide");
-        var column = power4.chooseColumn();
-        var lineEmpty = power4.returnLineCaseEmpty(column);
-    }
-    power4.playCase(players, lineEmpty, column);
-    power4.displayTab();
-    return power4.verifyEndGame(players);
+function playing(column) {
+    console.log("plop");
+    // var lineEmpty = -1;
+    // var column = -1;
+    // while (lineEmpty === -1 || column <= 0 || column > 7) {
+    //     console.log("choisir une colonne à un emplacement vide");
+    //     var column = power4.chooseColumn();
+    //     var lineEmpty = power4.returnLineCaseEmpty(column);
+    // }
+    // power4.playCase(players, lineEmpty, column);
+    // power4.displayTab();
+    // return power4.verifyEndGame(players);
 }
